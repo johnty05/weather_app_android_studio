f915fad3ea9d161c99e671c6e94fdba05502319e
johnty05 <johnty05>
2022-03-03 16:08:45 +0530

    Added the weather model and recycler adapter

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +30,19 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText editCityName;
     private ImageView bgImage, symbol, search;
     private RecyclerView forecast;
+    private ArrayList<WeatherModel> weatherModelArrayList;
+    private WeatherAdapter weatherAdapter;
+
+    // permission and loc manager
+    private LocationManager locManager;
+    private int PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // for fullscreen app
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_main);
         home = findViewById(R.id.idHome);
         progressBar = findViewById(R.id.idProgressBar);
@@ -34,5 +54,26 @@ public class MainActivity extends AppCompatActivity {
         symbol = findViewById(R.id.idSymbol);
         search = findViewById(R.id.idSearch);
         forecast = findViewById(R.id.idForecast);
+        weatherModelArrayList = new ArrayList<>();
+        weatherAdapter = new WeatherAdapter(this, weatherModelArrayList);
+        forecast.setAdapter(weatherAdapter);
+
+        // initialize loc manager
+        // check for perms grant
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE);
+        }
+
+        Location location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void getWeatherInfo(String cityName) {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=3253604179ee4a05868115644220103&q=" + cityName + "&days=1&aqi=no&alerts=no";
+    }
+
+    private String getCityName(double longitude, double latitude) {
+        String cityName = "SORRY NO SUCH CITY FOUND";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
     }
 }
--- app/src/main/java/com/example/weatherforecast/WeatherAdapter.java
+++ app/src/main/java/com/example/weatherforecast/WeatherAdapter.java
@@ -1,4 +1,72 @@
 package com.example.weatherforecast;
 
-public class WeatherAdapter extends  {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherModel> weatherModelArrayList;
+
+    public WeatherAdapter(Context context, ArrayList<WeatherModel> weatherModelArrayList) {
+        this.context = context;
+        this.weatherModelArrayList = weatherModelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WeatherAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        WeatherModel model = weatherModelArrayList.get(position);
+        holder.tempShow.setText(model.getTemp() + " C");
+        Picasso.with(context).load("http:".concat(model.getIcon())).into(holder.icon);
+        holder.humidity.setText(model.getHumidity() + " humid");
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat processedInput = new SimpleDateFormat("hh:mm aa");
+        try{
+            Date date = input.parse(model.getTime());
+            holder.time.setText(processedInput.format(date));
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherModelArrayList.size();
+    }
+
+    protected class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView time, tempShow, humidity;
+        private ImageButton icon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            time = itemView.findViewById(R.id.idTime);
+            tempShow = itemView.findViewById(R.id.idTempShow);
+            humidity = itemView.findViewById(R.id.idHumidity);
+            icon = itemView.findViewById(R.id.idIcon);
+
+        }
+    }
 }
